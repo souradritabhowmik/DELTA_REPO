b80d7de8d1d6962e8575f72eacc6745f89b79bc4
souradritabhowmik <12345678+souradritabhowmik@users.noreply.github.com>
2026-02-01 06:01:16 -0500

    final push fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
   output: 'standalone', // This helps Vercel package the app correctly
 };
 
